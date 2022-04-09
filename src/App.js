841971e4ef318b993f648612452221fd5feaeccf
onuryavuzmeral <onuryavuzmeral@hotmail.com>
2022-04-09 21:47:07 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ReactDemo from './pages/ReactDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <ReactDemo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/ReactDemo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReactDemo = () => (
+<h1>React Demo</h1>
+);
+
+export default ReactDemo;
